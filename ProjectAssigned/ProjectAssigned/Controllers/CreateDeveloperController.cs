a83913a5e7e2904ecf01a709449e9b5624766fec
usamamukhyer <usamamukhyer@gmail.com>
2021-08-27 13:12:46 +0500

    that is the initial and starting work of project from fiday

---
+++ ProjectAssigned/ProjectAssigned/Controllers/CreateDeveloperController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectAssigned.Controllers
+{
+    public class CreateDeveloperController : Controller
+    {
+        // GET: CreateDeveloper
+
+        public ActionResult AddDeveloper()
+        {
+            return View();
+        }
+
+
+        public ActionResult DeveloperList()
+        {
+            return View();
+        }
+    }
+}
